0bb94e84bee98f802aed16fd3808527e1f4d954a
customaddone <custom1485@gmail.com>
2019-11-13 06:34:06 +0000

    Delete on realtime

--- resources/js/vocabula.js
+++ resources/js/vocabula.js
@@ -5,25 +5,30 @@ var vm = new Vue({
       relatedWords: []
     }
   },
+
   mounted: function () {
-    var pathname= location.pathname;
-    var searchId = pathname.split("/");
-
-    axios.post('/api/words/' + searchId[3],{
-
-    }).then((response) => {
-      this.relatedWords = response.data;
-    }).catch((response) => {
-
-      console.log(response);
-    })
+    this.wordIndex();
   },
 
   methods: {
+   wordIndex: function () {
+     var pathname= location.pathname;
+     var searchId = pathname.split("/");
+
+     axios.post('/api/words/' + searchId[3],{
+
+     }).then((response) => {
+       this.relatedWords = response.data;
+     }).catch((response) => {
+
+       console.log(response);
+     })
+   },
+
    wordDelete: function (id) {
      axios.delete('/api/delwords/' + id)
      .then(() => {
-       this.wordIndex;
+       this.wordIndex();
      }).catch((response) => {
        console.log(response);
      });
