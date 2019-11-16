7e7369a80b935eb659e2adbd71c328de102567c8
customaddone <custom1485@docomo.ne.jp>
2019-11-16 23:47:37 +0000

    Organize route++

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::group(['middleware' => 'api'], function() {
+Route::group(['middleware' => ['api', 'modify']], function() {
   // wiki記事検索用
   Route::get("/homeSearch/{pass}", "WikiSearchController@homeSearch");
   // 辞書機能使用用
@@ -27,17 +27,17 @@ Route::group(['middleware' => 'api'], function() {
   // 記事ページ移動用
   Route::get("/articlesShow/{pass}", "WikiSearchController@articlesShow");
   // 記事のインポート用
-  Route::match(["get", "options"], '/get',  'ArticlesController@index')
-  Route::match(["get", "options"], '/find/{id?}',  'ArticlesController@find')
-  Route::match(["post", "options"], '/add',  'ArticlesController@store')
-  Route::match(["post", "options"], '/edit',  'ArticlesController@edit')
-  Route::match(["delete", "options"], '/del/{id}',  'ArticlesController@del')
+  Route::match(["get", "options"], '/get',  'ArticlesController@index');
+  Route::match(["get", "options"], '/find/{id?}',  'ArticlesController@find');
+  Route::match(["post", "options"], '/add',  'ArticlesController@store');
+  Route::match(["post", "options"], '/edit',  'ArticlesController@edit');
+  Route::match(["delete", "options"], '/del/{id}',  'ArticlesController@del');
 
-  Route::match(["post", "options"], '/words/add',  'WordsController@store')
+  Route::match(["post", "options"], '/words/add',  'WordsController@store');
 
-  Route::match(["post", "options"], '/words/{id}',  'WordsController@index')
+  Route::match(["post", "options"], '/words/{id}',  'WordsController@index');
 
-  Route::match(["post", "options"], '/editwords/{id}',  'WordsController@edit')
+  Route::match(["post", "options"], '/editwords/{id}',  'WordsController@edit');
 
-  Route::match(["delete", "options"], '/delwords/{id}', 'WordsController@del')
-})->middleware('modify');
+  Route::match(["delete", "options"], '/delwords/{id}', 'WordsController@del');
+});
