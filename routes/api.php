117d7a02e1a2bb51ddccdef9c9c66463a2ed3a73
customaddone <custom1485@docomo.ne.jp>
2019-11-16 23:43:48 +0000

    Organize route+

--- routes/api.php
+++ routes/api.php
@@ -28,25 +28,16 @@ Route::group(['middleware' => 'api'], function() {
   Route::get("/articlesShow/{pass}", "WikiSearchController@articlesShow");
   // 記事のインポート用
   Route::match(["get", "options"], '/get',  'ArticlesController@index')
-  ->middleware('modify');
   Route::match(["get", "options"], '/find/{id?}',  'ArticlesController@find')
-  ->middleware('modify');
   Route::match(["post", "options"], '/add',  'ArticlesController@store')
-  ->middleware('modify');  //←追記
   Route::match(["post", "options"], '/edit',  'ArticlesController@edit')
-  ->middleware('modify');
   Route::match(["delete", "options"], '/del/{id}',  'ArticlesController@del')
-  ->middleware('modify');
 
   Route::match(["post", "options"], '/words/add',  'WordsController@store')
-  ->middleware('modify');
 
   Route::match(["post", "options"], '/words/{id}',  'WordsController@index')
-  ->middleware('modify');
 
   Route::match(["post", "options"], '/editwords/{id}',  'WordsController@edit')
-  ->middleware('modify');
 
   Route::match(["delete", "options"], '/delwords/{id}', 'WordsController@del')
-  ->middleware('modify');
 })->middleware('modify');
