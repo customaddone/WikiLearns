b84c9f1fc98d0e9450ab2c372f0070280d20795e
customaddone <custom1485@docomo.ne.jp>
2019-11-17 08:25:14 +0000

    Use User class in phpunit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,6 +17,8 @@ use Illuminate\Support\Str;
 */
 // Userクラスを生成　フェイクデータを生成 return内の値をセット
 $factory->define(User::class, function (Faker $faker) {
+    static $password;
+    
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -5,9 +5,11 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\User;
 
 class HelloTest extends TestCase
 {
+
     /**
      * A basic feature test example.
      *
@@ -16,9 +18,17 @@ class HelloTest extends TestCase
     // 簡単なテスト実施 vendor/bin/phpunitで実施
     public function testHello()
     {
+        /*
+            phpunitの<php>欄に
+            <server name="DB_DSN" value="mysql:dbname=test_database;host=localhost" />
+            <server name="DB_USER" value="root" />
+            <server name="DB_PASSWD" value="secret" />
+            <server name="DB_DBNAME" value="wikiLearns" />
+            を記述する
+        */
         $this->assertTrue(0 < 1);
 
-        $arr = [];
-        $this->assertEmpty($arr);
+        $first = factory(User::class)->create();
+        $first->save();
     }
 }
